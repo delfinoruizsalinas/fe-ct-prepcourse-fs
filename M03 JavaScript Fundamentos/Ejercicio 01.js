68abf8a4a9983ed7f693ea93911bd5eb6a4ab4dd
Delfino Ruiz <druiz@apollo.mx>
2023-02-17 11:14:47 -0600

    Ejercicio 01 delfinoruizsalinas

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,19 +5,21 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = 'Delfino Ruiz Salinas';
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+const nuevoNumero = 34;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - null === 3;
 
+
 // Resuelve el siguiente problema matemático.
 const nuevaMultiplicacion = 10 * null === 40;
+false;
 
 // Resuelve el siguiente problema matemático.
 const nuevoModulo = 21 % 5 === null;
